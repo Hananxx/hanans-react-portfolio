84159d32d4a851385817897601c358cacfc5534f
Hanan <hanan.hakami@tuwaiq.edu.sa>
2021-07-14 19:57:16 +0300

    add: logo

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div>
+            <h1>Hanan</h1>
+            <h1>Hakami</h1>
+            <h3>Web Developer</h3>
+        </div>
+    )
+}
---
+++ src/components/Logo.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Logo() {
+    return (
+        <div>
+            <svg width="211" height="262" viewBox="0 0 211 262" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0H66.4259V197.438H34L0 166.5V0Z" fill="black" />
+                <path d="M0 0H66.4259V197.438H34L0 166.5V0Z" fill="url(#paint0_linear)" />
+                <path d="M72.287 0H105.5L136.213 30L138.713 261.472H105.5L72.287 233V0Z" fill="black" />
+                <path d="M72.287 0H105.5L136.213 30L138.713 261.472H105.5L72.287 233V0Z" fill="url(#paint1_linear)" />
+                <path d="M144.574 64.034H172L211 98.5V261.472H144.574V64.034Z" fill="black" />
+                <path d="M144.574 64.034H172L211 98.5V261.472H144.574V64.034Z" fill="url(#paint2_linear)" />
+                <path d="M1.9537 120.953V76.4851H130.898V120.953H1.9537Z" fill="black" />
+                <path d="M1.9537 120.953V76.4851H130.898V120.953H1.9537Z" fill="url(#paint3_linear)" />
+                <path d="M80.1019 184.987V140.519H183.648V184.987H80.1019Z" fill="black" />
+                <path d="M80.1019 184.987V140.519H183.648V184.987H80.1019Z" fill="url(#paint4_linear)" />
+                <defs>
+                    <linearGradient id="paint0_linear" x1="9" y1="-24" x2="168.5" y2="233" gradientUnits="userSpaceOnUse">
+                        <stop offset="0.0416667" stop-color="#FCEF96" />
+                        <stop offset="0.421875" stop-color="#FF86D3" />
+                        <stop offset="0.625" stop-color="#FA86D5" />
+                        <stop offset="1" stop-color="#968BFD" />
+                    </linearGradient>
+                    <linearGradient id="paint1_linear" x1="9" y1="-24" x2="168.5" y2="233" gradientUnits="userSpaceOnUse">
+                        <stop offset="0.0416667" stop-color="#FCEF96" />
+                        <stop offset="0.421875" stop-color="#FF86D3" />
+                        <stop offset="0.625" stop-color="#FA86D5" />
+                        <stop offset="1" stop-color="#968BFD" />
+                    </linearGradient>
+                    <linearGradient id="paint2_linear" x1="9" y1="-24" x2="168.5" y2="233" gradientUnits="userSpaceOnUse">
+                        <stop offset="0.0416667" stop-color="#FCEF96" />
+                        <stop offset="0.421875" stop-color="#FF86D3" />
+                        <stop offset="0.625" stop-color="#FA86D5" />
+                        <stop offset="1" stop-color="#968BFD" />
+                    </linearGradient>
+                    <linearGradient id="paint3_linear" x1="9" y1="-24" x2="168.5" y2="233" gradientUnits="userSpaceOnUse">
+                        <stop offset="0.0416667" stop-color="#FCEF96" />
+                        <stop offset="0.421875" stop-color="#FF86D3" />
+                        <stop offset="0.625" stop-color="#FA86D5" />
+                        <stop offset="1" stop-color="#968BFD" />
+                    </linearGradient>
+                    <linearGradient id="paint4_linear" x1="9" y1="-24" x2="168.5" y2="233" gradientUnits="userSpaceOnUse">
+                        <stop offset="0.0416667" stop-color="#FCEF96" />
+                        <stop offset="0.421875" stop-color="#FF86D3" />
+                        <stop offset="0.625" stop-color="#FA86D5" />
+                        <stop offset="1" stop-color="#968BFD" />
+                    </linearGradient>
+                </defs>
+            </svg>
+
+        </div>
+    )
+}
